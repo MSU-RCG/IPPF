beb957b3ae45373d2c2bccae807f2520a7ec9776
Pol Llovet <pol.llovet@gmail.com>
2011-03-14 00:40:35 -0600

    Update spec_helper with DM specific config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,11 +17,13 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
 
+  config.before(:suite) {DataMapper.auto_migrate!}
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 end
