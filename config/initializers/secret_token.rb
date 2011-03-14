15e21f614190113e995580dbc300cad732b6629b
Pol Llovet <pol.llovet@gmail.com>
2011-03-14 00:29:42 -0600

    Initial commit of app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ippf::Application.config.secret_token = '02c8bf80a7cc1a5b67d4597e267e6f2209aeb6bf1aabd958418764b9dd4da1c10a8ee3c0d807102b7b8bcd725dc4694d83e2ccbfd09da1556ea86dde902a140d'
