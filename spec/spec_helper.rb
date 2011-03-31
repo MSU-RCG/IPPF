231bfa88290e25ee835710b5c5891b930d6d8ba6
Pol Llovet <pol.llovet@gmail.com>
2011-03-30 23:24:04 -0600

    Include the devise test helper once

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@ RSpec.configure do |config|
   config.mock_with :rspec
 
   config.before(:suite) {DataMapper.auto_migrate!}
+  config.include Devise::TestHelpers, :type => :controller
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
