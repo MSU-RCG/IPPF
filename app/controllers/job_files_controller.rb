351231e4b816be7df14e306bcea08b19f5f19bca
Pol Llovet <pol.llovet@gmail.com>
2011-04-12 14:38:17 -0600

    Attempt to make the file upload work (still fail)

--- app/controllers/job_files_controller.rb
+++ app/controllers/job_files_controller.rb
@@ -3,10 +3,12 @@ class JobFilesController < ApplicationController
 
   # POST /job_files/:uuid
   def create
+    puts "******STARTING UPLOAD******"
     @jobfile = JobFile.new(:name => params[:name])
     @jobfile.job_uuid = params[:uuid]
-    @jobfile.file = params[:file]  
+    @jobfile.file = params[:file]
     status = @jobfile.save ? :created : :bad_request
+    logger.info @jobfile.errors.full_messages
     render :nothing => true, :status => status
   end
 end
