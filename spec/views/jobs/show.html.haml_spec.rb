7520edc1278fb91be5e6ce877819756a861f1a38
Pol Llovet <pol.llovet@gmail.com>
2011-04-18 15:45:39 -0600

    Update the look/feel for the views

--- spec/views/jobs/show.html.haml_spec.rb
+++ spec/views/jobs/show.html.haml_spec.rb
@@ -5,7 +5,7 @@ describe "jobs/show.html.haml" do
     @job = assign(:job, stub_model(Job,
       :name => "Name",
       :job_type => "Job Type",
-      :user_id => 1,
+      :user => assign(:user, stub_model(User, :email => 'foo@bat.com')),
       :coordinates => "Coordinates",
       :status => "Status",
       :notes => "MyText"
@@ -19,7 +19,7 @@ describe "jobs/show.html.haml" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Job Type/)
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/1/)
+    rendered.should match(/foo@bat.com/)
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Coordinates/)
     # Run the generator again with the --webrat flag if you want to use webrat matchers
