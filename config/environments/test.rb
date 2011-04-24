6bfd113c9764cffbbcd2ff48df935f390414a3d9
Pol Llovet <pol.llovet@gmail.com>
2011-04-23 20:38:46 -0600

    Add a proper mail host to the text environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,6 +24,9 @@ Ippf::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { 
+    :host => 'localhost:3000',
+    :from => "ippf-support@rcg.msu.montana.edu" }
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
